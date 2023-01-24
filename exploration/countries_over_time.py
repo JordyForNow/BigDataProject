8e69e2ccc5dfde67316a392170a2afa37e36465c
Joost Loohuis <j.e.loohuis@student.utwente.nl>
2023-01-24 12:07:04 +0100

    Updated countries_over_time.py

--- exploration/countries_over_time.py
+++ exploration/countries_over_time.py
@@ -1,8 +1,11 @@
+import math
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 from os import path
 from matplotlib import pyplot as plt
+import matplotlib as mpl
+import numpy as np
 
 in_folder = '../Data/referrer-monthly-host-countries_processed'
 out_file = '../Data/all_months_countries.csv'
@@ -25,6 +28,9 @@ for year in range(2014, 2023):
 all_months = pd.concat(monthly_dataframes)
 
 result = all_months.pivot(index=['year', 'month'], values='count', columns='country')
+# years_index = result.index.to_series().apply(lambda x : f'{x[0]}' if x[1] == 1 else None)
+years_index = result.index.to_frame()
+years_ticks = years_index[years_index['month'] == 1]
 
 countries_all_time = result.sum().astype('int').sort_values(ascending=False)
 
@@ -37,7 +43,7 @@ countries_all_time_labels[10:] = None
 
 pieplot = countries_all_time.plot.pie(labels=countries_all_time_labels, title='Country distribution over whole timespan')
 pieplot.figure.show()
-pieplot.figure.savefig('../Plots/all_time_countries.png', transparent=True)
+# pieplot.figure.savefig('../Plots/all_time_countries.png', transparent=True)
 
 alltime_top_countries = countries_all_time.reset_index()[:5]['country']
 alltime_top_all_months = result[alltime_top_countries]
@@ -48,30 +54,33 @@ alltime_top_all_months['Total'] = all_visits_all_months
 lineplot = alltime_top_all_months.plot.line(figsize=(20, 10))
 lineplot.grid(axis='y')
 lineplot.figure.show()
-lineplot.figure.savefig('../Plots/top_alltime_countries_nonlog.png')
+# lineplot.figure.savefig('../Plots/top_alltime_countries_nonlog.png')
 
 lineplot = alltime_top_all_months.plot.line(logy=True, figsize=(20, 10))
 lineplot.grid(axis='y')
 lineplot.figure.show()
-lineplot.figure.savefig('../Plots/top_alltime_countries.png')
+# lineplot.figure.savefig('../Plots/top_alltime_countries.png')
 
 alltime_std = all_visits_all_months.std()
 
 # detrend by taking the difference with the previous month
-derivative = all_visits_all_months[1:] - all_visits_all_months.values[:-1]
-der_plot = derivative.plot()
-plt.hlines(alltime_std, xmin=0, xmax=derivative.size-1, colors='C1')
+differencing = all_visits_all_months[1:] - all_visits_all_months.values[:-1]
+dif_plot = differencing.plot()
+plt.hlines(alltime_std, xmin=0, xmax=differencing.size - 1, colors='C1')
 plt.show()
 
 # take the moving average over a year
-der_moving_average = derivative.rolling(window=12).mean()
-der_moving_std = derivative.rolling(window=12).std()
+dif_moving_average = differencing.rolling(window=12).mean()
+dif_moving_std = differencing.rolling(window=12).std()
 
-derivative.plot(figsize=(20, 10), title='Traffic Derivative')
-der_moving_average.plot()
-der_moving_std.plot()
+differencing.plot(figsize=(20, 10), title='Traffic detrended', xticks=[12*i for i in range(0, 10)])
+dif_moving_average.plot()
+# dif_moving_std.plot()
+plt.hlines([differencing.std(), -differencing.std()], 0, differencing.size - 1, colors='C1')
+# plt.hlines(0, 0, differencing.size - 1, colors='black')
+plt.grid(visible=True)
 
-plt.legend(['Traffic', 'Year average', 'Year std'])
+plt.legend(['Traffic', 'Std'])
 plt.show()
 
 # look at country distribution over time
@@ -79,19 +88,43 @@ plt.show()
 sorted_countries_key = countries_all_time.reset_index()['country']
 result_dist = result.div(all_visits_all_months, axis=0)
 sorted_result_dist = result_dist[sorted_countries_key]
-sorted_result_dist.plot.bar(stacked=True, legend=False, figsize=(20, 10), title='Country Traffic Distribution', color=['20BF55'])
+sorted_result_dist.plot.bar(stacked=True, legend=False, figsize=(20, 10), title='Country Traffic Distribution')
 
 # handles, labels = plt.axes().get_legend_handles_labels()
 plt.legend(sorted_countries_key[:30], bbox_to_anchor=(1.04, 1), loc='upper left')
-plt.savefig('../Plots/country_distribution.png')
+# plt.savefig('../Plots/country_distribution.png')
 plt.show()
+
+plt.axes().xaxis.set_minor_locator(mpl.ticker.AutoMinorLocator(1))
+plt.minorticks_on()
 
 moving_average = all_visits_all_months.rolling(5, center=True).mean()
 detrended = all_visits_all_months - moving_average
 # detrended_ma = detrended.rolling(3, center=True).mean()
 detrended_ma = detrended
-detrended_ma.plot()
+detrended_ma.plot(xticks=[12*i for i in range(0, 10)], figsize=(20, 10))
 detrended_std = detrended_ma.std()
 plt.hlines(0, 0, detrended_ma.size - 1, colors='black')
 plt.hlines([detrended_std, -detrended_std], 0, detrended_ma.size-1, colors='C1')
+plt.grid(axis='x', which='both')
+plt.show()
+
+
+def label_filter(row):
+    year = row['year']
+    if not math.isnan(year):
+        return f"{year:.0f}"
+    else:
+        return ""
+
+
+labels = years_index.where(years_index['month'] == 1).apply(label_filter, result_type='reduce', axis=1)
+
+fig, ax = plt.subplots()
+fig.set_size_inches(40, 10)
+# im = ax.imshow(sorted_result_dist.values)
+im = ax.pcolormesh(sorted_result_dist.values, norm='log', cmap='plasma')
+ax.set_xticks(np.arange(sorted_result_dist.columns.size), labels=sorted_result_dist.columns, rotation=90, ha="right",
+         rotation_mode="anchor")
+ax.set_yticks(np.arange(labels.size), labels=labels)
 plt.show()
