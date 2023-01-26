78cc8fba343f31c114c73ff6277f1a362fae26f8
Joost Loohuis <j.e.loohuis@student.utwente.nl>
2023-01-26 14:33:34 +0100

    Fix all hosts processing

--- processing/post_processing/all_hosts_post_processing.py
+++ processing/post_processing/all_hosts_post_processing.py
@@ -1,25 +1,15 @@
 import pandas as pd
 from post_processing import post_process
 
-in_path = '/Users/fabin/Downloads/TopCountries_WarSpike.csv'
-
-out_path = '/Users/fabin/Downloads/TopCountries_WarSpike1.csv'
+in_path = '../../Data/all_hosts.csv'
+out_path = '../../Data/all_hosts_post-processed.csv'
 
 df = pd.read_csv(in_path)
-df = df.iloc[: , 1:]
-df = df.drop(columns=['country_name'])
-df1 = pd.read_csv(in_path)
-
-print(df1.head())
-df2 = df1.drop(columns=['host'])
-print(df2.head())
 
 test_df = post_process(df)
-print(test_df)
-# test_df.to_csv(out_path, index=False)
+
+test_df = test_df.groupby('host').sum().reset_index()
+
+test_df.to_csv(out_path, index=False)
 
 host = test_df['host']
-df2['host'] = host
-print(df2)
-
-df2.to_csv(out_path, index=False)
