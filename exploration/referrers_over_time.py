93e3d277c2efc5fe7fa3ff13520307d473c0ccf2
Joost Loohuis <j.e.loohuis@student.utwente.nl>
2023-01-26 15:56:51 +0100

    Annotate referrers_over_time.py

--- exploration/referrers_over_time.py
+++ exploration/referrers_over_time.py
@@ -8,7 +8,10 @@ import matplotlib as mpl
 import numpy as np
 
 in_folder = '../Data/referrer-monthly-host-countries_processed'
-out_file = '../Data/all_months_countries.csv'
+
+###
+# Coalesce the data for all months into a single dataframe
+###
 
 monthly_dataframes = []
 
@@ -27,20 +30,28 @@ for year in range(2014, 2023):
 
 all_months = pd.concat(monthly_dataframes)
 
+# Shift the indexes so rows are months and columns are referrers
 result = all_months.pivot(index=['year', 'month'], values='count', columns='host')
 
+# Remove the websdr itself, as it takes up the bulk of referrals and is not very interesting
 result_no_websdr = result.drop('websdr.ewi.utwente.nl', axis=1)
 
+# Get traffic per referrer for the whole duration of the data set
 hosts_all_time = result_no_websdr.sum().astype('int').sort_values(ascending=False)
 
+# Sort on total amount of referrals made
 sorted_result_no_websdr = result_no_websdr[hosts_all_time.index]
 
+# Turn this into percentages using the total amount of visits in the data set
 all_time_visits = hosts_all_time.sum()
 hosts_all_time_percentage = hosts_all_time / all_time_visits * 100
 
+###
+# Aggregate smallest referrers, as they are not interesting individually
+###
 topn = 250
 
-print(hosts_all_time_percentage[:topn].sum())
+print('Top', topn, 'make up', hosts_all_time_percentage[:topn].sum(), '% of traffic')
 
 top_result_no_wbsdr = sorted_result_no_websdr.iloc[:, :topn]
 
@@ -49,8 +60,12 @@ top_result_no_wbsdr['REST'] = rest_result
 
 all_visits_per_month = top_result_no_wbsdr.sum(axis=1)
 
+# Turn this into percentages using the total amount of visits
 top_result_no_wbsdr_dist = top_result_no_wbsdr.div(all_visits_per_month, axis=0)
 
+###
+# Pieplot of the distribution of traffic over referrers
+###
 # hosts_all_time_labels = hosts_all_time.reset_index()['host']
 # top_hosts_all_time_labels = hosts_all_time.reset_index()['host']
 # top_hosts_all_time_labels[20:] = None
@@ -58,34 +73,18 @@ top_result_no_wbsdr_dist = top_result_no_wbsdr.div(all_visits_per_month, axis=0)
 # pieplot = hosts_all_time.plot.pie(labels=top_hosts_all_time_labels)
 # plt.show()
 
+###
+# Look at referrer distribution over time
+###
 years_labels = result.index.to_series().apply(lambda x : f"{x[0]}" if x[1] == 1 else None)
 
-fig, ax = plt.subplots()
-fig.set_size_inches(40, 15)
-im = ax.pcolormesh(top_result_no_wbsdr_dist.values, norm='log', cmap='plasma')
-ax.set_xticks(np.arange(top_result_no_wbsdr_dist.columns.size), labels=top_result_no_wbsdr_dist.columns, rotation=90, ha="right",
-         rotation_mode="anchor")
-ax.set_yticks(np.arange(years_labels.size), labels=years_labels)
-cbar = ax.figure.colorbar(im, ax=ax)
-plt.tight_layout()
-plt.show()
-
+# Find in how many months each referrer occurs
 emptiness = top_result_no_wbsdr_dist.count().sort_values(ascending=False)
-es_top_results = top_result_no_wbsdr_dist[emptiness.index]
 
+# Sort first on the amount of months a referrer occurs in, then alltime referrals
 emptiness.name = 'emptiness'
 hosts_all_time.name = 'hat'
 sort_key = pd.concat([emptiness, hosts_all_time], axis=1).loc[top_result_no_wbsdr.columns].sort_values(['emptiness', 'hat'], ascending=False, na_position='first')
-
-fig, ax = plt.subplots()
-fig.set_size_inches(40, 15)
-im = ax.pcolormesh(es_top_results.values, norm='log', cmap='plasma')
-ax.set_xticks(np.arange(es_top_results.columns.size), labels=es_top_results.columns, rotation=90, ha="right",
-         rotation_mode="anchor")
-ax.set_yticks(np.arange(years_labels.size), labels=years_labels)
-cbar = ax.figure.colorbar(im, ax=ax)
-plt.tight_layout()
-plt.show()
 
 st_result_no_websdr = top_result_no_wbsdr[sort_key.index]
 
@@ -103,6 +102,7 @@ cbar.ax.set_ylabel('Percentage of traffic', fontsize=fontsize)
 plt.tight_layout()
 plt.show()
 
+# Sort only by alltime referrals
 hosts_all_time_contribution = top_result_no_wbsdr_dist.sum().sort_values(ascending=False)
 atc_top_result = top_result_no_wbsdr_dist[hosts_all_time_contribution.index]
 
@@ -116,12 +116,12 @@ cbar = ax.figure.colorbar(im, ax=ax)
 plt.tight_layout()
 plt.show()
 
+###
+# Plot the count of unique referrers each month
+###
 amount_of_hosts = result.count(axis=1)
 amount_of_hosts.plot()
 plt.show()
-
-amount_of_hosts_spike = result.loc[(2019, 7)]
-amount_of_hosts_spike = amount_of_hosts_spike[~amount_of_hosts_spike.isna()]
 
 host_distribution_start = result_no_websdr.loc[(2014, 4)]
 host_distribution_start = host_distribution_start[~host_distribution_start.isna()]
@@ -129,8 +129,8 @@ host_distribution_start = host_distribution_start[~host_distribution_start.isna(
 host_distribution_end = result_no_websdr.loc[(2022, 1)]
 host_distribution_end = host_distribution_end[~host_distribution_end.isna()]
 
+# As the distribution is very logarithmic, make the buckets in log space
 max_referrals = int(max(host_distribution_start.max(), host_distribution_end.max()))
-
 exponential_buckets = np.logspace(0, np.log10(max_referrals + 1000))
 
 start_hist = host_distribution_start.plot.hist(bins=exponential_buckets)
