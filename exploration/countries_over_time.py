c586610c5ff86be93529704c535f1e01e5a57c96
Joost Loohuis <j.e.loohuis@student.utwente.nl>
2023-01-26 15:10:19 +0100

    Annotate countries_over_time.py

--- exploration/countries_over_time.py
+++ exploration/countries_over_time.py
@@ -8,7 +8,10 @@ import matplotlib as mpl
 import numpy as np
 
 in_folder = '../Data/referrer-monthly-host-countries_processed'
-out_file = '../Data/all_months_countries.csv'
+
+###
+# Coalesce the data for all months into a single dataframe
+###
 
 monthly_dataframes = []
 
@@ -27,17 +30,24 @@ for year in range(2014, 2023):
 
 all_months = pd.concat(monthly_dataframes)
 
+# Shift the indexes so rows are months and columns are countries
 result = all_months.pivot(index=['year', 'month'], values='count', columns='country')
-# years_index = result.index.to_series().apply(lambda x : f'{x[0]}' if x[1] == 1 else None)
-years_index = result.index.to_frame()
-years_ticks = years_index[years_index['month'] == 1]
 
+years_index = result.index.to_frame()
+
+# Get traffic per country for the whole duration of the data set
 countries_all_time = result.sum().astype('int').sort_values(ascending=False)
 
+# Turn this into percentages using the total amount of visits in the data set
 all_time_visits = countries_all_time.sum()
 countries_all_time_percentage = countries_all_time / all_time_visits * 100
-print(countries_all_time_percentage[:10].sum())
+print('Top make up', countries_all_time_percentage[:10].sum(), '%')
 
+
+###
+# Pieplot of the distribution of traffic over countries
+###
+# Only label the top 10
 countries_all_time_labels = countries_all_time.reset_index()['country']
 countries_all_time_labels[10:] = None
 
@@ -45,11 +55,15 @@ pieplot = countries_all_time.plot.pie(labels=countries_all_time_labels, title='C
 pieplot.figure.show()
 # pieplot.figure.savefig('../Plots/all_time_countries.png', transparent=True)
 
+###
+# Graph traffic for the top countries over time
+###
 alltime_top_countries = countries_all_time.reset_index()[:5]['country']
-alltime_top_all_months = result[alltime_top_countries]
+alltime_top_all_months = result.loc[:, alltime_top_countries]
 
+# Add the total visits each month for context
 all_visits_all_months = result.sum(axis=1).astype('int')
-alltime_top_all_months['Total'] = all_visits_all_months
+alltime_top_all_months.loc[:, 'Total'] = all_visits_all_months
 
 lineplot = alltime_top_all_months.plot.line(figsize=(20, 10))
 lineplot.grid(axis='y')
@@ -61,15 +75,13 @@ lineplot.grid(axis='y')
 lineplot.figure.show()
 # lineplot.figure.savefig('../Plots/top_alltime_countries.png')
 
-alltime_std = all_visits_all_months.std()
-
+###
+# Remove trend for first analysis
+###
 # detrend by taking the difference with the previous month
 differencing = all_visits_all_months[1:] - all_visits_all_months.values[:-1]
-dif_plot = differencing.plot()
-plt.hlines(alltime_std, xmin=0, xmax=differencing.size - 1, colors='C1')
-plt.show()
 
-# take the moving average over a year
+# take the moving average and standard deviation over a year
 dif_moving_average = differencing.rolling(window=12).mean()
 dif_moving_std = differencing.rolling(window=12).std()
 
@@ -90,18 +102,7 @@ plt.tight_layout()
 plt.legend(['Traffic', 'Std'])
 plt.show()
 
-# look at country distribution over time
-# Sort countries by alltime visits
-sorted_countries_key = countries_all_time.reset_index()['country']
-result_dist = result.div(all_visits_all_months, axis=0)
-sorted_result_dist = result_dist[sorted_countries_key]
-# sorted_result_dist.plot.bar(stacked=True, legend=False, figsize=(20, 10), title='Country Traffic Distribution')
-#
-# # handles, labels = plt.axes().get_legend_handles_labels()
-# plt.legend(sorted_countries_key[:30], bbox_to_anchor=(1.04, 1), loc='upper left')
-# # plt.savefig('../Plots/country_distribution.png')
-# plt.show()
-
+# Higher def plot
 plt.axes().xaxis.set_minor_locator(mpl.ticker.AutoMinorLocator(1))
 plt.minorticks_on()
 
@@ -116,16 +117,16 @@ plt.hlines([detrended_std, -detrended_std], 0, detrended_ma.size-1, colors='C1')
 plt.grid(axis='x', which='both')
 plt.show()
 
+###
+# Look at country distribution over time
+###
+# Sort countries by alltime visits
+sorted_countries_key = countries_all_time.reset_index()['country']
+result_dist = result.div(all_visits_all_months, axis=0)
+sorted_result_dist = result_dist[sorted_countries_key]
 
-def label_filter(row):
-    year = row['year']
-    if not math.isnan(year):
-        return f"{year:.0f}"
-    else:
-        return ""
-
-
-labels = years_index.where(years_index['month'] == 1).apply(label_filter, result_type='reduce', axis=1)
+# Label only the first month of each year, but show ticks for all months
+labels = result.index.to_series().apply(lambda x : f'{x[0]}' if x[1] == 1 else None)
 
 fontsize = 17
 fig, ax = plt.subplots()
@@ -144,11 +145,11 @@ cbar.ax.set_ylabel('Percentage of traffic', fontsize=fontsize)
 plt.tight_layout()
 plt.show()
 
-# Plot sudden spike countries
+##
+# Plot sudden spikes in country traffic, as indicated by dark columns
+##
 visual_lh_countries = ['RE', 'MZ', 'NC', 'CK', 'PF']
 visual_lh_result = result[visual_lh_countries]
-
-
 
 plot = visual_lh_result.plot(alpha=0.7)
 plot.hlines(visual_lh_result.median(), 0, visual_lh_result.index.size - 1, linestyles='dashed', colors=['C0', 'C1', 'C2', 'C3', 'C4'])
